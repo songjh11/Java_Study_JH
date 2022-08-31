1d830cbb65592a942e90a10a64d529d1dbd9308a
songjh11 <jhmelody91@gmail.com>
2022-08-31 13:56:17 +0900

    831-6

--- src/main/webapp/resources/JS/dom_5.js
+++ src/main/webapp/resources/JS/dom_5.js
@@ -16,7 +16,32 @@ const d1 = document.getElementById("d1");
 d1.addEventListener("blur", function(){
     console.log("d1blur");
     console.log(d1.value.length);
-    if(d1.value.length<3){
+    let v=d1.value;
+    if(v.length<4){
         d1.focus();
     }
 })
+
+const d3 = document.getElementById("d3");
+
+d3.addEventListener("change", function(){
+    console.log("change발생")
+})
+
+const d5 = document.getElementById("d5");
+
+d5.addEventListener("change", function(){
+    console.log("d5d5");
+})
+
+const d4 = document.getElementById("d4");
+
+d4.addEventListener("keyup", function(){
+    console.log("keyup");
+    let f = d4.value;
+    console.log(f.length);
+    ss.innerHTML = f.length+"천원";
+})
+
+
+
