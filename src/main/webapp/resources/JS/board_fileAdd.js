b1a55c7a44f7490b73bfb2883028ef80eb01ee15
songjh11 <jhmelody91@gmail.com>
2022-09-01 17:29:44 +0900

    901-4

--- src/main/webapp/resources/JS/board_fileAdd.js
+++ src/main/webapp/resources/JS/board_fileAdd.js
@@ -6,7 +6,9 @@
                 const tableAdd = document.getElementById("tableAdd");
                 const fileAdd = document.getElementById("fileAdd");
                 const tbody = document.getElementById("tbody");
+                const addFiles = document.getElementById("addFiles");
                 let result = 0;
+                let idx = 0;
                 
                 fileAdd.addEventListener("click", function(){
                     if(result<5){
@@ -15,17 +17,56 @@
                         let input = document.createElement("input");
                         let file = document.createAttribute("type");
                         let text = document.createTextNode("파일: ");
+                        let button = document.createElement("button");
+                        let btype = document.createAttribute("type");
+                        let btext = document.createTextNode("파일 삭제");
+
+                        
                         file.value="file";
-                        input.setAttribute("name", "files");
                         input.setAttributeNode(file);
-                        td.setAttribute("colspan", "2");
+                        
+                        file = document.createAttribute("name");
+                        file.value = "files";
+                        input.setAttributeNode(file);
+                        
+                        let span = document.createAttribute("colspan");
+                        span.value = "2";
+                        td.setAttributeNode(span);
                         td.appendChild(text);
                         td.appendChild(input);
+
+                        btype.value = "button";
+                        button.setAttributeNode(btype);
+
+                        btype = document.createAttribute("class");
+                        btype.value = "del btn btn-danger";
+                        button.setAttributeNode(btype);
+                        
+                        btype = document.createAttribute("id");
+                        btype.value = idx;
+                        button.setAttributeNode(btype);
+                        button.appendChild(btext);
+                        
+                        td.appendChild(button);
+
+                        let trid = document.createAttribute("id");
+                        trid.value="file"+idx;
+                        tr.setAttributeNode(trid);
                         tr.appendChild(td);
                         tbody.append(tr);
+
                         result++;
+                        idx++;
                     } else{
                         alert("최대 5개까지 추가 가능");
                     }
+                })
+                tableAdd.addEventListener("click", function(event){
+                   if(event.target.classList[0]=="del"){
+                       alert(event.target.innerHTML);
+                        tableAdd.removeChild(0);
+                                      console.log(event.target);
+
+                   }
                 })
             }
